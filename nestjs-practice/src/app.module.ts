9012a08b48c1e7c505f0abad8b0faa29f891e69e
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-11-03 15:50:39 +0900

    いけそう

--- nestjs-practice/src/app.module.ts
+++ nestjs-practice/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { HelloResolver } from './resolvers/hello.resolver';
 
@@ -11,9 +10,6 @@ import { HelloResolver } from './resolvers/hello.resolver';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
-      definitions: {
-        path: join(process.cwd(), 'src/generated/graphql.ts'),
-      },
     }),
   ],
   controllers: [AppController],
--- nestjs-practice/src/generated/graphql.ts
+++ nestjs-practice/src/generated/graphql.ts
@@ -1,4 +1,3 @@
-
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -8,16 +7,17 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export interface IQuery {
-    users(): User[] | Promise<User[]>;
-    user(id: string): Nullable<User> | Promise<Nullable<User>>;
+export abstract class IQuery {
+  abstract users(): User[] | Promise<User[]>;
+
+  abstract user(id: string): Nullable<User> | Promise<Nullable<User>>;
 }
 
-export interface User {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
+export class User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 type Nullable<T> = T | null;
