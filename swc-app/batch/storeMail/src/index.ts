e6b6d84c9c9f3773cebdca9d0bf154287e932ccc
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2024-06-02 13:38:17 +0900

    修正

--- swc-app/batch/storeMail/src/index.ts
+++ swc-app/batch/storeMail/src/index.ts
@@ -1,8 +1,14 @@
 import { connectMail } from "@utils/connectMail";
 
 const main = async () => {
-  const res = await connectMail("hoge@hoge.com", "xxxxxxxx");
-  console.log(res);
+  console.log("Starting main function");
+  try {
+    const res = await connectMail("hoge@hoge.com", "xxxxxxxx");
+    console.log(res);
+  } catch (error) {
+    console.error("Error in connectMail:", error);
+  }
+  console.log("Hello World");
 };
 
 main();
